fcf884d93637eaf2683e115058d33148b9baf890
adriacabeza <adriacabeza@hotmail.com>
2019-08-20 17:12:46 +0200

    adding spinner

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-import * as tf from '@tensorflow/tfjs';
+//import * as tf from '@tensorflow/tfjs';
 var file = null
 
 function thisFileUpload() {
     file = document.getElementById("file").click();
+    if(file != null){
+        button = document.getElementById("boton");
+        button.style.visibility="hidden";
+        spinner = document.getElementById("loader");
+        spinner.style.visibility="visible";
+    }
 };
 
-const model = await tf.loadGraphModel(FICAR ALGU)
-model.predict(tf.browser.fromPixels(file))
+// const model = await tf.loadGraphModel(FICAR ALGU)
+// model.predict(tf.browser.fromPixels(file))
