80b6529f6e83dd33dd36f3422cca15ded93aa87f
lord_santanna <adriacabezasantanna@gmail.com>
2019-08-28 13:21:53 +0200

    Update delete_white.py

--- dataset/delete_white.py
+++ dataset/delete_white.py
@@ -1,27 +1,27 @@
 import argparse
-
+import numpy as np
 from PIL import Image
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--image', type=str())
+parser.add_argument('--image', type=str)
 args = parser.parse_args()
 
 
 img = Image.open(args.image)
 img = img.convert("RGBA")
 
-pixdata = img.load()
+pixdata = img.getdata()
 
-def bigger((r,g,b,a)):
-	if r > 250 and g > 250 and b > 250 and a > 250:
+def bigger(r,g,b):
+	if r > 250 and g > 250 and b > 250:
 		return True
-	else
+	else:
 		return False
-
-width, height = image.size
-for y in range(height):
-	for x in range(width):
-		if bigger(pixdata[x, y]):
-			pixdata[x, y] = (255, 255, 255, 0)
-
+newData = []
+for item in pixdata:
+	if bigger(item[0],item[1],item[2]):
+		newData.append((255, 255, 255, 0))
+	else:
+		newData.append(item)	
+img.putdata(newData)
 img.save("img2.png", "PNG")
